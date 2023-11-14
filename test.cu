4e36d72c8ce5deb16d5922f2551a92528f237c03
LittleHanhan <1627582619@qq.com>
2023-11-14 15:23:32 +0800

    update

--- test.cu
+++ test.cu
@@ -10,13 +10,13 @@ void initval(int s[], int n) {
     }
 }
 
-__global__ void sumArrayOnGpu(int* ga, int* gb, int* gres, int tx, int ty, int bx, int by) {
-    int i = (blockIdx.y * by + blockIdx.x) * tx * ty + threadIdx.y * ty + threadIdx.x;
+__global__ void sumTwoArrayOnGpu(int* ga, int* gb, int* gres, int tx, int ty, int bx, int by) {
+    int i = (blockIdx.y * bx + blockIdx.x) * tx * ty + threadIdx.y * tx + threadIdx.x;
     gres[i] = ga[i] + gb[i];
 }
 
-void test(int tx, int ty) {
-    int l = 32 * 1024;
+void sumTwoMatrix(int tx, int ty) {
+    int l = 1024;
     int n = l * l;
     int nbyte = sizeof(int) * n;
 
@@ -33,20 +33,55 @@ void test(int tx, int ty) {
 
     cudaMemcpy(ga, a, nbyte, cudaMemcpyHostToDevice);
     cudaMemcpy(gb, b, nbyte, cudaMemcpyHostToDevice);
+    cudaMemcpy(gres, a, nbyte, cudaMemcpyHostToDevice);
 
     dim3 block(tx, ty);
     dim3 grid(l / tx, l / ty);
-
-    sumArrayOnGpu<<<grid, block>>>(ga, gb, gres, tx, ty, l / tx, l / ty);
+    cout << tx << " " << ty << " " << l / tx << " " << l / ty;
+    sumTwoArrayOnGpu<<<grid, block>>>(ga, gb, gres, tx, ty, l / tx, l / ty);
     cudaMemcpy(res, gres, nbyte, cudaMemcpyDeviceToHost);
 
-    cout << res[10666] << endl;
+    cout << res[n - l] << endl;
 
     cudaFree(ga);
     cudaFree(gb);
     cudaFree(gres);
     delete[] a;
     delete[] b;
+    delete[] res;
+}
+
+__global__ void reduce1(int* ga, int n) {
+    int i = blockIdx.x * blockDim.x + threadIdx.x;
+    for (int stride = n / 2; stride > 0; stride = (stride + 1) / 2) {
+        if (i < stride) {
+            ga[i] += ga[i + stride];
+        }
+        ga[stride] = 0;
+        if (stride == 1)
+            break;
+    }
+}
+
+void sumArray(int tx) {
+    int n = 1024;
+    int nbyte = sizeof(int) * n;
+    int* a = new int[n];
+    int* res = new int;
+    initval(a, n);
+
+    int* ga;
+    cudaMalloc(&ga, nbyte);
+    dim3 block(tx);
+    dim3 grid(n / tx);
+    cout << tx << " " << n / tx;
+    reduce1<<<grid, block>>>(ga, n);
+    cudaMemcpy(res, ga, sizeof(int), cudaMemcpyDeviceToHost);
+
+    cout << *res << endl;
+
+    cudaFree(ga);
+    delete[] a;
     delete[] res;
 }
 
@@ -63,6 +98,6 @@ int main(int argc, char** argv) {
 
     int tx = atoi(argv[1]);
     int ty = atoi(argv[2]);
-    cout << tx << " " << ty;
-    test(tx, ty);
+    // sumTwoMatrix(tx, ty);
+    sumArray(tx);
 }
