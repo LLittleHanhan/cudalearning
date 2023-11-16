226587a0288a69618299f8dc151489d6c8a7b9bc
LittleHanhan <1627582619@qq.com>
2023-11-16 17:37:19 +0800

    update

---
+++ memary.cu
@@ -0,0 +1,30 @@
+#include <cuda_runtime.h>
+#include <device_launch_parameters.h>
+#include <cstdio>
+#include <cstring>
+#include <iostream>
+using namespace std;
+
+__device__ int ga = 8;
+__device__ int gb;
+
+__global__ void var() {
+    int a = 0;          // 寄存器
+    int s[1000] = {0};  // 本地内存
+    printf("%d %d %d %d %d", threadIdx.x, a, s[0], ga, gb);
+}
+
+int main(int argc, char** argv) {
+    int dev = 0;
+    cudaSetDevice(dev);
+    cudaDeviceProp prop;
+    cudaGetDeviceProperties(&prop, dev);
+    cout << prop.name << endl;
+    cout << "num of mutipro:" << prop.multiProcessorCount << endl;
+    cout << "max num threads of per block:" << prop.maxThreadsPerBlock << endl;
+    cout << "max num threads of per mutipro:" << prop.maxThreadsPerMultiProcessor << endl;
+    cout << "max num warps of per mutipro:" << prop.maxThreadsPerMultiProcessor / 32 << endl;
+    int test = 100;
+    cudaMemcpyToSymbol(gb, &test, sizeof(int));
+    var<<<1, 8>>>();
+}
--- sumTwoMatrix.cu
+++ sumTwoMatrix.cu
@@ -10,8 +10,8 @@ void initval(int s[], int n) {
     }
 }
 
-__global__ void sumTwoArrayOnGpu(int* ga, int* gb, int* gres, int tx, int ty, int bx, int by) {
-    int i = (blockIdx.y * bx + blockIdx.x) * tx * ty + threadIdx.y * tx + threadIdx.x;
+__global__ void sumTwoArrayOnGpu(int* ga, int* gb, int* gres) {
+    int i = (blockIdx.y * gridDim.x + blockIdx.x) * blockDim.x * blockDim.y + threadIdx.y * blockDim.x + threadIdx.x;
     gres[i] = ga[i] + gb[i];
 }
 
@@ -38,7 +38,7 @@ void sumTwoMatrix(int tx, int ty) {
     dim3 block(tx, ty);
     dim3 grid(l / tx, l / ty);
     cout << tx << " " << ty << " " << l / tx << " " << l / ty;
-    sumTwoArrayOnGpu<<<grid, block>>>(ga, gb, gres, tx, ty, l / tx, l / ty);
+    sumTwoArrayOnGpu<<<grid, block>>>(ga, gb, gres);
     cudaMemcpy(res, gres, nbyte, cudaMemcpyDeviceToHost);
 
     cout << res[n - l] << endl;
@@ -48,40 +48,6 @@ void sumTwoMatrix(int tx, int ty) {
     cudaFree(gres);
     delete[] a;
     delete[] b;
-    delete[] res;
-}
-
-__global__ void reduce1(int* ga, int n) {
-    int i = blockIdx.x * blockDim.x + threadIdx.x;
-    for (int stride = n / 2; stride > 0; stride = (stride + 1) / 2) {
-        if (i < stride) {
-            ga[i] += ga[i + stride];
-        }
-        ga[stride] = 0;
-        if (stride == 1)
-            break;
-    }
-}
-
-void sumArray(int tx) {
-    int n = 1024;
-    int nbyte = sizeof(int) * n;
-    int* a = new int[n];
-    int* res = new int;
-    initval(a, n);
-
-    int* ga;
-    cudaMalloc(&ga, nbyte);
-    dim3 block(tx);
-    dim3 grid(n / tx);
-    cout << tx << " " << n / tx;
-    reduce1<<<grid, block>>>(ga, n);
-    cudaMemcpy(res, ga, sizeof(int), cudaMemcpyDeviceToHost);
-
-    cout << *res << endl;
-
-    cudaFree(ga);
-    delete[] a;
     delete[] res;
 }
 
@@ -98,6 +64,5 @@ int main(int argc, char** argv) {
 
     int tx = atoi(argv[1]);
     int ty = atoi(argv[2]);
-    // sumTwoMatrix(tx, ty);
-    sumArray(tx);
+    sumTwoMatrix(tx, ty);
 }
